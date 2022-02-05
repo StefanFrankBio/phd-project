ac9555b8e177c31ff04eeea26fd4d0653c73e964
Stefan <stefan@Stefans-MacBook-Air.fritz.box>
2022-02-05 17:19:04 +0100

    Align simulated sequences

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import itertools
+from Bio import SeqIO, Align
+
+def read_reference(filepath):
+    with open(filepath) as handle:
+        for record in SeqIO.parse(handle, "fasta"):
+            return record.seq
+
+
+def read_mutated_sequences(filepath):
+    mutated_sequences = []
+    with open(filepath) as handle:
+        for record in SeqIO.parse(handle, "fasta"):
+            mutated_sequences.append(record.seq)
+    return mutated_sequences
+
+
 def split_codons(sequence):
     overhang = len(sequence) % 3
     if overhang != 0:
@@ -16,7 +33,6 @@ def single_substitutions(codon):
 
 
 def build_trans_table():
-    import itertools
     nucs = "TCAG"
     aminos = "FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG"
     codons = (itertools.product(nucs, nucs, nucs))
@@ -27,18 +43,31 @@ def build_trans_table():
 def count_synonymous_sites(bool_list):
     return [sum(bool_list[x*3:(x+1)*3])/3 for x in range(3)]
 
+
+def align_pair(reference, mutated):
+    aligner = Align.PairwiseAligner()
+    aligner.mode = "global"
+    aligner.open_gap_score = -1
+    alignment = aligner.align(reference, mutated)
+    alignment = str(alignment[0])[:-1].split("\n")
+    return alignment
+
+
 def main():
     trans_table = build_trans_table()
     synonymous_sites = []
-    with open("geneS.fasta") as f:
-        next(f)
-        sequence = f.read().replace("\n", "")
-    codons = split_codons(sequence)
+    reference = read_reference("simulated_reference.fasta")
+    codons = split_codons(reference)
     for codon in codons:
         sns = single_substitutions(codon)
         translations = [trans_table[s] for s in sns]
         is_syn = [amino == trans_table[codon] for amino in translations]
         synonymous_sites += count_synonymous_sites(is_syn)
+
+    mutated_sequences = read_mutated_sequences("simulated_mutations.fasta")
+    for seq in mutated_sequences[:1]:
+        alignment = align_pair(reference, seq)
+
 
 if __name__ == "__main__":
     main()
---
+++ simulate_sequences.py
@@ -0,0 +1,35 @@
+import itertools
+import random
+
+def generate_reference():
+    nucleotides = "TCAG"
+    codons = itertools.product(nucleotides, nucleotides, nucleotides)
+    codons = ["".join(tpl) for tpl in codons]
+    random.shuffle(codons)
+    reference = "".join(codons)
+    return reference
+
+
+def mutate_sequence(sequence):
+    nucleotides = "TCAG"
+    idx = random.randint(0, len(sequence)-1)
+    nucleotides = nucleotides.replace(sequence[idx], "")
+    mutation = random.choice(nucleotides)
+    return sequence[:idx] + mutation + sequence[idx+1:]
+
+
+def main():
+    reference = generate_reference()
+    mutation = reference
+    with open("simulated_reference.fasta", "w") as output:
+        print(">simulated_reference", file=output)
+        print(reference, file=output)
+    with open("simulated_mutations.fasta", "w") as output:
+        for i in range(100):
+            mutation = mutate_sequence(mutation)
+            print(f">simulated_mutation_{i}", file=output)
+            print(mutation, file=output)
+
+
+if __name__ == "__main__":
+    main()
